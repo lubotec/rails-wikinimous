c57843aac556db10e56c48d022f48a3f062d7c74
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-05-14 16:45:17 +0200

    Ex2 finished backend

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new           # GET /restaurants/new
+    @article = Article.new
+  end
+
+  def create        # POST /restaurants
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit          # GET /restaurants/:id/edit
+    @article = Article.find(params[:id])
+  end
+
+  def update        # PATCH /restaurants/:id
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy       # DELETE /restaurants/:id
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:article).permit(:title, :content)
+  end
+end
